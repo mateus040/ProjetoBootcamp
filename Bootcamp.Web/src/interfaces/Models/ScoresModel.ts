31fde0b4c610159190307441b5e22b4bf6acf7ab
mateus040 <mateusgabrielmoreno264@gmail.com>
2024-09-27 19:50:02 -0300

    feat: adicionando conexão com endpoint de pontução

---
+++ Bootcamp.Web/src/interfaces/Models/ScoresModel.ts
@@ -0,0 +1,5 @@
+export default interface ScoresModel {
+  id: number;
+  nome: string;
+  pontuacao: number;
+}
--- Bootcamp.Web/src/pages/pontuacao/index.tsx
+++ Bootcamp.Web/src/pages/pontuacao/index.tsx
@@ -1,7 +1,31 @@
 import { Link } from "react-router-dom";
 import { Layout } from "../../components/layout/main";
+import { useEffect, useState } from "react";
+import ScoresModel from "../../interfaces/Models/ScoresModel";
+import axios from "axios";
+import apiErrorHandler from "../../services/api-error-handler";
 
 export default function Pontuacao() {
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const [points, setPoints] = useState<ScoresModel[]>([]);
+
+  const fetchPoints = () => {
+    setLoading(true);
+
+    axios
+      .get("http://localhost:8000/api/accounts/scores/")
+      .then((response) => {
+        setPoints(response.data);
+      })
+      .catch(apiErrorHandler)
+      .finally(() => setLoading(false));
+  };
+
+  useEffect(() => {
+    fetchPoints();
+  }, []);
+
   return (
     <Layout>
       <div className="min-h-screen flex flex-col justify-between py-6 px-8">
@@ -10,25 +34,27 @@ export default function Pontuacao() {
             Pontuação geral
           </p>
 
-          <div className="mt-8 flex flex-col items-center lg:items-start justify-center lg:justify-start">
-            <div className="space-y-5">
-              <div className="flex items-center text-4xl">
-                {/* Nome do usuario */}
-                <p>Jogador 1:</p>
+          {loading && <p className="text-lg">Carregando...</p>}
 
-                {/* Pontuação do usuário */}
-                <p className="ms-5">35</p>
-              </div>
+          {!loading && (
+            <div className="mt-8 flex flex-col items-center lg:items-start justify-center lg:justify-start">
+              <div className="space-y-5">
+                {points.map((point, _) => (
+                  <div className="flex items-center text-4xl" key={point.id}>
+                    {/* Nome do usuario */}
+                    <p>{point.nome}:</p>
 
-              <div className="flex items-center text-4xl">
-                {/* Nome do usuario */}
-                <p>Jogador 1:</p>
-
-                {/* Pontuação do usuário */}
-                <p className="ms-5">35</p>
+                    {/* Pontuação do usuário */}
+                    <p className="ms-5">{point.pontuacao}</p>
+                  </div>
+                ))}
               </div>
             </div>
-          </div>
+          )}
+
+          {!loading && points.length === 0 && (
+            <p className="text-lg">Nenhuma pontuação encontrada.</p>
+          )}
         </div>
 
         <Link
